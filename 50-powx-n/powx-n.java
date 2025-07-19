7dd50987ca4880bbde35866ef3d659ac6be62744
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-07-19 22:08:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (10.25%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if(N<0){
+            x = 1/x;
+            N = -N;
+        }
+
+        if(n==0){
+            return 1;
+        }
+
+        double val = myPow(x, (int)(N/2));
+
+        if(N%2 == 0){
+            return val*val;
+        } else{
+            return val*val*x;
+        }
+
+    }
+}
