17bb6e58cdf4bd04d2a4454cf914122ba01beaf4
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-07-24 23:35:16 +0530

    Time: 1 ms (99.48%) | Memory: 44 MB (79.28%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,37 +1,72 @@
 class Solution {
+    // public int compress(char[] chars) {
+
+    //     int x =0;
+    //     String a =new String();
+    //     int count =1;
+
+    //     if(chars.length == 1){
+    //         a +=chars[0];
+    //         return 1;
+    //     }
+
+    //     for(int i=1; i<chars.length; i++){
+    //         if(chars[x] == chars[i]){
+    //             count++;
+    //         }else{
+    //             a +=chars[x];
+    //             if(count>1){
+    //                 a += Integer.toString(count);
+    //                 count =1;
+    //             }
+    //             x = i;
+
+    //         }
+    //     }
+    //     a +=chars[x];
+    //     if(count>1){
+    //         a += Integer.toString(count);
+    //     }
+
+    //     for(int i=0; i<a.length(); i++){
+    //         chars[i] = a.charAt(i);
+    //     }
+
+    //     return a.length();
+    // }
+
     public int compress(char[] chars) {
-
-        int x =0;
-        String a =new String();
-        int count =1;
-
-        if(chars.length == 1){
-            a +=chars[0];
+        int update = 0;
+        int x = 0;
+        int count = 1;
+        if (chars.length == 1) {
             return 1;
         }
-
-        for(int i=1; i<chars.length; i++){
-            if(chars[x] == chars[i]){
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[x] == chars[i]) {
                 count++;
-            }else{
-                a +=chars[x];
-                if(count>1){
-                    a += Integer.toString(count);
-                    count =1;
+            } else {
+                chars[update++] = chars[x];
+                if (count > 1) {
+                    String cou = Integer.toString(count);
+                    for (int j = 0; j < cou.length(); j++) {
+                        chars[update++] = cou.charAt(j);
+                    }
+                    count = 1;
                 }
                 x = i;
 
             }
         }
-        a +=chars[x];
-        if(count>1){
-            a += Integer.toString(count);
+
+        chars[update++] = chars[x];
+        if (count > 1) {
+            String cou = Integer.toString(count);
+            for (int j = 0; j < cou.length(); j++) {
+                chars[update++] = cou.charAt(j);
+            }
         }
 
-        for(int i=0; i<a.length(); i++){
-            chars[i] = a.charAt(i);
-        }
-
-        return a.length();
+        return update;
     }
 }
