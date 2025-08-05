47c81cdf5b75b4e35038455ebacb62e421726051
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-08-06 02:14:35 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (15.38%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -3,20 +3,20 @@ class Solution {
         // first rule: leftmoset in order basket[j] > fruits[i]
         // second rule: once used basket can't be used
         // third rule: if it is not possible to place then don't place
-        boolean[] flag = new boolean[baskets.length];
-        Arrays.fill(flag, true);
         int count =0;
         for(int i=0; i<fruits.length; i++){
+            boolean flag = true;
             for(int j=0; j< baskets.length; j++){
-                if(baskets[j] >= fruits[i] && flag[j]){
-                    flag[j] = false;
-                    count++;
+                if(baskets[j] >= fruits[i]){
+                    baskets[j] = 0;
+                    flag = false;
                     break;
                 }
             }
+            if(flag) count++;
         }
 
-        return baskets.length-count;
+        return count;
         
     }
 }
