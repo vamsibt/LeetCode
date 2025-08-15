fc2d2bb614c34a040872e13f09dc3dc517f6d8e0
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-08-15 21:47:07 +0530

    Time: 6 ms (72.80%) | Memory: 45.3 MB (60.53%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,29 +1,34 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        LinkedList<Integer> list[]=new LinkedList[numCourses];
-        for (int i=0;i<numCourses;i++){
-            list[i]=new LinkedList<>();
+        LinkedList<Integer> list[] = new LinkedList[numCourses];
+
+        for(int i=0; i<numCourses; i++){
+            list[i] = new LinkedList<>();
         }
-        int dip[]=new int[numCourses];
-        for (int i=0;i<prerequisites.length;i++){
+
+        int[] dip = new int[numCourses];
+
+        for(int i=0; i<prerequisites.length; i++){
             list[prerequisites[i][1]].add(prerequisites[i][0]);
             dip[prerequisites[i][0]]++;
+
         }
-        Queue<Integer> q=new LinkedList<>();
-        for (int i=0;i<numCourses;i++){
-            if (dip[i]==0) q.add(i);
+        Queue<Integer> que = new LinkedList<>();
+        for(int i=0; i<numCourses; i++){
+            if(dip[i] == 0) que.add(i);
         }
-        int c=0;
-        while(!q.isEmpty()){
-            int crnt=q.poll();
-            c++;
-            for (int i:list[crnt]){
-                dip[i]--;
-                if (dip[i]==0){
-                    q.add(i);
-                }
+
+        int count=0;
+        while(!que.isEmpty()){
+            int course = que.poll();
+            count++;
+            for(int c: list[course]){
+                dip[c]--;
+                if(dip[c] == 0) que.add(c);
             }
+
         }
-        return (numCourses==c);
+
+        return (numCourses==count);
     }
 }
