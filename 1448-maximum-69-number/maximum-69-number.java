7b168b6812968c6590eb71b04d807b00272f00e4
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-08-18 01:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (44.59%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,23 +1,34 @@
 class Solution {
-    public int maximum69Number (int number) {
-        String a = String.valueOf(number);
-        int[] num = new int[a.length()];
-        for(int i=0; i<a.length(); i++){
-            num[i] = a.charAt(i) - '0';
-        }
+    // public int maximum69Number (int number) {
+    //     String a = String.valueOf(number);
+    //     int[] num = new int[a.length()];
+    //     for(int i=0; i<a.length(); i++){
+    //         num[i] = a.charAt(i) - '0';
+    //     }
 
-        int max = Integer.MIN_VALUE;
-        for(int i=0; i<num.length; i++){
-            if(num[i] == 6){
-                num[i] = 9;
+    //     int max = Integer.MIN_VALUE;
+    //     for(int i=0; i<num.length; i++){
+    //         if(num[i] == 6){
+    //             num[i] = 9;
+    //             break;
+    //         }
+    //     }
+    //     int result =0;
+    //     for(int i=0; i<num.length; i++){
+    //         result = (result*10) + num[i];
+    //     }
+
+    //     return result;
+    // }
+    public int maximum69Number (int number) {
+        StringBuilder sb = new StringBuilder(String.valueOf(number));
+        for(int i=0; i<sb.length(); i++){
+            if(sb.charAt(i) == '6') {
+                sb.setCharAt(i, '9');
                 break;
             }
         }
-        int result =0;
-        for(int i=0; i<num.length; i++){
-            result = (result*10) + num[i];
-        }
 
-        return result;
+        return Integer.parseInt(sb.toString());
     }
 }
