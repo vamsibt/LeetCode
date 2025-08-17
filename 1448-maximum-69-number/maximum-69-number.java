afd91226a41128d8c25de08fd322bc788ccee1e8
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-08-18 00:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (56.62%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximum69Number (int number) {
+        String a = String.valueOf(number);
+        int[] num = new int[a.length()];
+        for(int i=0; i<a.length(); i++){
+            num[i] = a.charAt(i) - '0';
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<num.length; i++){
+            if(num[i] == 6){
+                num[i] = 9;
+                break;
+            }
+        }
+        int result =0;
+        for(int i=0; i<num.length; i++){
+            result = (result*10) + num[i];
+        }
+
+        return result;
+    }
+}
