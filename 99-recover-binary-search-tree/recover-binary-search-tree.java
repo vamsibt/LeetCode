8e963d3d68ac176ddb60309fe898409e3309342d
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-09-03 02:53:04 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (12.76%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void recoverTree(TreeNode root) {
+        ArrayList<TreeNode> arr = new ArrayList<>();
+        inorder(root,arr);
+        TreeNode left = null;
+        int idx= 0;
+        TreeNode right = null;
+
+        for(int i=0; i<arr.size()-1; i++){
+            if(arr.get(i).val > arr.get(i+1).val){
+                if(left == null){
+                    left = arr.get(i);
+                    idx = i;
+                } else{
+                    right = arr.get(i+1);
+                    break;
+                }
+            }
+        }
+
+        if(right ==null){
+            right = arr.get(idx+1);
+        }
+
+        int temp = left.val;
+        left.val = right.val;
+        right.val = temp;
+
+    }
+
+    public void inorder(TreeNode root, ArrayList<TreeNode> arr){
+        if(root == null){
+            return;
+        }
+        inorder(root.left, arr);
+        arr.add(root);
+        inorder(root.right, arr);
+    }
+}
