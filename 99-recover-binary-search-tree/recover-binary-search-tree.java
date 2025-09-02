e75a7ec4ae1d0c58467a167280b07dc7fbab1a90
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-09-03 03:04:12 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (21.13%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -14,41 +14,71 @@
  * }
  */
 class Solution {
+    // public void recoverTree(TreeNode root) {
+    //     ArrayList<TreeNode> arr = new ArrayList<>();
+    //     inorder(root,arr);
+    //     TreeNode left = null;
+    //     int idx= 0;
+    //     TreeNode right = null;
+
+    //     for(int i=0; i<arr.size()-1; i++){
+    //         if(arr.get(i).val > arr.get(i+1).val){
+    //             if(left == null){
+    //                 left = arr.get(i);
+    //                 idx = i;
+    //             } else{
+    //                 right = arr.get(i+1);
+    //                 break;
+    //             }
+    //         }
+    //     }
+
+    //     if(right ==null){
+    //         right = arr.get(idx+1);
+    //     }
+
+    //     int temp = left.val;
+    //     left.val = right.val;
+    //     right.val = temp;
+
+    // }
+
+    // public void inorder(TreeNode root, ArrayList<TreeNode> arr){
+    //     if(root == null){
+    //         return;
+    //     }
+    //     inorder(root.left, arr);
+    //     arr.add(root);
+    //     inorder(root.right, arr);
+    // }
+
+    TreeNode prev = null;
+    TreeNode left = null;
+    TreeNode right = null;
     public void recoverTree(TreeNode root) {
-        ArrayList<TreeNode> arr = new ArrayList<>();
-        inorder(root,arr);
-        TreeNode left = null;
-        int idx= 0;
-        TreeNode right = null;
-
-        for(int i=0; i<arr.size()-1; i++){
-            if(arr.get(i).val > arr.get(i+1).val){
-                if(left == null){
-                    left = arr.get(i);
-                    idx = i;
-                } else{
-                    right = arr.get(i+1);
-                    break;
-                }
-            }
+        inorder(root);
+        if(right == null){
+            right = prev;
         }
-
-        if(right ==null){
-            right = arr.get(idx+1);
-        }
-
         int temp = left.val;
         left.val = right.val;
         right.val = temp;
-
     }
 
-    public void inorder(TreeNode root, ArrayList<TreeNode> arr){
+    public void inorder(TreeNode root){
         if(root == null){
             return;
         }
-        inorder(root.left, arr);
-        arr.add(root);
-        inorder(root.right, arr);
+        inorder(root.left);
+        if(prev != null && prev.val > root.val){
+            if(left == null){
+                left = prev;
+                right = root;
+            } else{
+                right = root;
+            }
+        }
+        prev = root;
+        inorder(root.right);
     }
 }
