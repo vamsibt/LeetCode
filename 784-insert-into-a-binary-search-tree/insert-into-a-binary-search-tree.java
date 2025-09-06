b368ef712193ce3611cad04ff4cd03ee5b81334b
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-09-06 12:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (29.41%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -19,16 +19,9 @@ class Solution {
             return new TreeNode(val);
         }
         if(root.val < val){
-            if(root.right == null){
-                root.right = new TreeNode(val);
-                
-            }
-            insertIntoBST(root.right, val);
+            root.right = insertIntoBST(root.right, val);
         }else if(root.val > val){
-            if(root.left == null){
-                root.left = new TreeNode(val);
-            }
-            insertIntoBST(root.left, val);
+            root.left = insertIntoBST(root.left, val);
         }
 
         return root;
