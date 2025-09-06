0f5f6266944f13bfa15279ed74d48c9c252664db
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-09-06 13:18:50 +0530

    Time: 1 ms (13.48%) | Memory: 45.4 MB (47.11%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -14,32 +14,59 @@
  * }
  */
 class Solution {
-    int sum =0;
+    //SOL 1
+
+    // int sum =0;
+    // public TreeNode convertBST(TreeNode root) {
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     inorder(root, map);
+    //     build(root, map);
+    //     return root; 
+    // }
+
+    // public void inorder(TreeNode root, HashMap<Integer, Integer> map){
+    //     if(root == null){
+    //         return;
+    //     }
+    //     inorder(root.left,map);
+    //     sum += root.val;
+    //     map.put(root.val, sum);
+    //     inorder(root.right,map);
+    // }
+
+    // public void build(TreeNode root, HashMap<Integer, Integer> map){
+    //     if(root == null){
+    //         return;
+    //     }
+
+    //     root.val = sum-map.get(root.val)+root.val;
+    //     build(root.left, map);
+    //     build(root.right, map);
+    // }
+
+    //SOL 2
     public TreeNode convertBST(TreeNode root) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        inorder(root, map);
-        build(root, map);
+        int total = sum(root);
+        build(root, total);
         return root;
     }
+    int currentSum =0;
 
-    public void inorder(TreeNode root, HashMap<Integer, Integer> map){
+    public void build(TreeNode root, int total){
         if(root == null){
             return;
         }
-        inorder(root.left,map);
-        sum += root.val;
-        map.put(root.val, sum);
-        inorder(root.right,map);
+        build(root.left, total);
+        currentSum += root.val;
+        root.val = total-currentSum+root.val;
+        build(root.right, total);
     }
 
-    public void build(TreeNode root, HashMap<Integer, Integer> map){
+    public int sum(TreeNode root){
         if(root == null){
-            return;
+            return 0;
         }
-
-        root.val = sum-map.get(root.val)+root.val;
-        build(root.left, map);
-        build(root.right, map);
+        return root.val+sum(root.left)+sum(root.right);
     }
 
     
